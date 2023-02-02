45ef0bc04d001a9543ac3e4dae6303951822f749
Jesier <jesiersantiago@gmail.com>
2023-02-02 14:16:17 -0600

    Models have been made

---
+++ FilmSpot/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FilmSpot.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Info { get; set; }
+        public string Image { get; set; }
+        public string Trailer { get; set; }
+        public DateTime RealeaseDate { get; set; }
+        public Genre GenreId { get; set; }
+        public bool UserCreated { get; set; }
+    }
+}
---
+++ FilmSpot/Models/UserCatalog.cs
@@ -0,0 +1,11 @@
+﻿namespace FilmSpot.Models
+{
+    public class UserCatalog
+    {
+        public int Id { get; set; }
+        public int UserProfileId { get; set; }
+        public int MovieId { get; set; }
+        public bool Favorite { get; set; }
+        public bool Later { get; set; }
+    }
+}
