d3190b7e4d0c14e334aa505e290de6391e32af6d
Jesier <jesiersantiago@gmail.com>
2023-02-06 15:58:25 -0600

    Got the movie data fetched now i need to work on the cards

---
+++ FilmSpot/Client/src/components/MovieList.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Hello() {
+  const [movies, setMovies] = useState([])
+
+const apiKey = "efd0ff32160fa99cfcda71cd93209624"
+
+  useEffect(() => {
+    //fetching the movies from the MovieDB
+    axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`)
+    .then(res => {
+      setMovies(res.data.results)
+      
+    }).catch(err => {console.log(err)}) 
+  },[])
+  
+  
+  return (
+    <>
+    <div className="MoviesContainer">
+      {movies.map(movie => {
+        return movie.video
+      })}
+    </div>
+    </>
+  );
+}
