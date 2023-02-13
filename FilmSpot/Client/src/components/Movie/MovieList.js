41e01193c2dba7f066d10e5a13effb2427eddb2c
Jesier <jesiersantiago@gmail.com>
2023-02-13 09:51:03 -0600

    saving the changes for tomorrow

--- FilmSpot/Client/src/components/Movie/MovieList.js
+++ FilmSpot/Client/src/components/Movie/MovieList.js
@@ -7,17 +7,15 @@ export default function MovieList() {
   const [url, setUrl] = useState('https://api.themoviedb.org/3/movie/top_rated?api_key=efd0ff32160fa99cfcda71cd93209624&language=en-US&page=1');
   const [query, setQuery] = useState('');
   const [page, setPage] = useState(1);
-  const [favorite, setFavorite] = useState([])
 
   const apiKey = "efd0ff32160fa99cfcda71cd93209624"
 
 
 
   const searchMovie = (e) => {
-     //e.preventDefault();
-     const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=efd0ff32160fa99cfcda71cd93209624&language=en-US&query=${query}&page=1&include_adult=false`;
-     setUrl(searchUrl);
-     //setQuery("");
+    const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=efd0ff32160fa99cfcda71cd93209624&language=en-US&query=${query}&page=1&include_adult=false`;
+    setUrl(searchUrl);
+    //setQuery("");
   };
 
   const handlePageChange = (newPage) => {
@@ -25,30 +23,29 @@ export default function MovieList() {
     setUrl(`https://api.themoviedb.org/3/movie/top_rated?api_key=efd0ff32160fa99cfcda71cd93209624&language=en-US&page=${newPage}
     `);
     window.scrollTo(0, 0);
-}
+  }
 
   useEffect(() => {
     //fetching the movies from the MovieDB
-    const fetch = async () => {
-    const res = await axios.get(`${url}&offset=${(page - 1) * 20}&limit=20`);
+    axios.get(`${url}&offset=${(page - 1) * 20}&limit=20`)
+      .then(res => {
         setMovies(res.data.results);
         setPage(res.data.page);
-      }
-      fetch()
+      })
   }, [url, page])
 
 
   return (
     <>
       <form>
-      <div className="movie_search_bar">
-        <input type="search" placeholder='Search Here' id="movie_search"
-          className="form-control"
-          value={query}
-          onChange={e => setQuery(e.target.value)}
-          onKeyPress={searchMovie}
-        />
-      </div>
+        <div className="movie_search_bar">
+          <input type="search" placeholder='Search Here' id="movie_search"
+            className="form-control"
+            value={query}
+            onChange={e => setQuery(e.target.value)}
+            onKeyPress={searchMovie}
+          />
+        </div>
       </form>
       <div className="MoviesContainer">
         {movies.map(movie => {
@@ -57,9 +54,9 @@ export default function MovieList() {
         })}
       </div>
       <div className="page_buttons">
-                <button id="prev_button" className="btn btn-light" onClick={() => handlePageChange(page - 1)}>Prev</button>
-                <button id="next_button" className="btn btn-light" onClick={() => handlePageChange(page + 1)}>Next</button>
-            </div>
+        <button id="prev_button" className="btn btn-light" onClick={() => handlePageChange(page - 1)}>Prev</button>
+        <button id="next_button" className="btn btn-light" onClick={() => handlePageChange(page + 1)}>Next</button>
+      </div>
     </>
   );
 }
